7dcf2f192189b51b6f8c4d43d3b6bc835bfdeb8b
Marco Antonio Islas Cruz <markuz@islascruz.org>
2013-09-12 13:36:58 -0500

    Trying to make it better

--- poster_resources/mail.py
+++ poster_resources/mail.py
@@ -83,7 +83,7 @@ class Message(object):
                 self.image_list.append((filename, payload))
                 
     def get_text(self):
-        return "\n".join(self.text_list)
+        return ("\n".join(self.text_list)).decode("utf8")
     
     def get_images(self):
         return self.image_list
